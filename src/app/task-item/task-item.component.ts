cd09501b805df0c684890494433fe753296f3a19
Mark Jabel Frias <friasmarkjabel@gmail.com>
2024-04-19 02:01:17 +0800

    Change button icon to make UI consistent

--- src/app/task-item/task-item.component.ts
+++ src/app/task-item/task-item.component.ts
@@ -5,11 +5,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatPseudoCheckboxModule } from '@angular/material/core';
 import { LocalTaskService } from '../services/local-task/local-task.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [MatListModule, MatIconModule, MatCheckboxModule],
+  imports: [MatListModule, MatIconModule, MatCheckboxModule, MatButtonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.scss'
 })
